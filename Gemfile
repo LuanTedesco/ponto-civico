19f2537958142bc7e615aa4e6e71461ff568cb8e
LuanTedesco <luanfelipe2040@gmail.com>
2023-12-02 15:01:34 -0300

    changed db to sqlite3

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem "jbuilder"
 
 gem 'devise'
 
+gem 'sqlite3'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_02_173339) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +22,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_02_173339) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
