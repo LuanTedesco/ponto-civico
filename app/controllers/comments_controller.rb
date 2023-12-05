12f2839c484d8fe14d9562e743a138990285613b
LuanTedesco <luanfelipe2040@gmail.com>
2023-12-04 21:25:43 -0300

    added comments to show post and style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,27 +2,24 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_comment, only: %i[ show edit update destroy ]
 
-  # GET /comments or /comments.json
   def index
     @comments = Comment.all
   end
 
-  # GET /comments/1 or /comments/1.json
   def show
   end
 
-  # GET /comments/new
   def new
     @comment = Comment.new
   end
 
-  # GET /comments/1/edit
   def edit
   end
 
-  # POST /comments or /comments.json
   def create
     @comment = Comment.new(comment_params)
+    @comment.post_id = 1
+    @comment.user_id = current_user.id
 
     respond_to do |format|
       if @comment.save
@@ -35,7 +32,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1 or /comments/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
@@ -48,7 +44,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1 or /comments/1.json
   def destroy
     @comment.destroy!
 
@@ -59,12 +54,10 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def comment_params
       params.require(:comment).permit(:content, :status, :user_id, :post_id)
     end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,25 +2,23 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
+    @comments = Comment.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -36,7 +34,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -49,7 +46,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy!
 
@@ -60,12 +56,10 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :body, :status, :user_id)
     end
