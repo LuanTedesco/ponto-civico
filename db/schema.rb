e05f0666b40bdbb9fecf7546fea34c8c7d7b6f67
Luan Tedesco <luanfelipe2040@gmail.com>
2023-12-13 10:07:26 -0300

    changed color table categories

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_12_231425) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -88,15 +89,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_12_231425) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "name"
-    t.text "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -115,11 +107,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_12_231425) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "notifications", "users"
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
-  add_foreign_key "profiles", "users"
 end
